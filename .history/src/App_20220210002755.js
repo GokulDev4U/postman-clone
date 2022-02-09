444af9be7ec5b9268e96ec87bf9674c72032e65f
GokulDev4U <sreeharipisharody678@gmail.com>
2022-02-10 01:10:07 +0530

    finished

--- .history/src/App_20220210002755.js
+++ .history/src/App_20220210002755.js
@@ -1,12 +1,11 @@
+import "./App.css";
 import Home from "./components/Home";
-
-import DataProvider from "./context/DataProvider";
 
 function App() {
   return (
-    <DataProvider>
+    <div className="App">
       <Home />
-    </DataProvider>
+    </div>
   );
 }
 
